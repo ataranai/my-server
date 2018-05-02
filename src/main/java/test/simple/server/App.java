e6a814ed8ff8495d03a1c8c2602a190c8a982b19
enagai <enagai@localhost.localdomain>
2018-05-02 11:41:07 +0900

    first commit

---
+++ src/main/java/test/simple/server/App.java
@@ -0,0 +1,29 @@
+package test.simple.server;
+
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class App 
+{
+    	private static final int SERVER_PORT = 10006;
+	 
+	public static void main(String args[]) {
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+	    try {
+	      serverSocket = new ServerSocket(SERVER_PORT);
+	      while (true) {
+	    	  System.out.println("-- simple SampleServer start");
+	    	  socket = serverSocket.accept();
+	    	  socket.close();
+	    	  System.out.println("-- simple SampleServer end");
+	      }
+	    }catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
