d5817b172dc05f7aae81ae99cad6cd9512051a48
enagai <enagai@dhcp-217-153.nrt.redhat.com>
2018-05-14 12:00:23 +0900

    Add reply message

--- src/main/java/test/simple/server/App.java
+++ src/main/java/test/simple/server/App.java
@@ -1,29 +1,40 @@
 package test.simple.server;
 
-import java.net.Socket;
-import java.net.ServerSocket;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.IOException;
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
 public class App 
 {
-    	private static final int SERVER_PORT = 8080;
-	 
-	public static void main(String args[]) {
-		ServerSocket serverSocket = null;
-		Socket socket = null;
-	    try {
-	      serverSocket = new ServerSocket(SERVER_PORT);
-	      while (true) {
-	    	  System.out.println("-- simple SampleServer start");
-	    	  socket = serverSocket.accept();
-	    	  socket.close();
-	    	  System.out.println("-- simple SampleServer end");
-	      }
-	    }catch (IOException e) {
-			e.printStackTrace();
+    private static final int SERVER_PORT = 8080;
+    
+    public static String messages[]  = {
+	"********************************",
+	"****  Welcome to My Server! ****",
+	"********************************",
+    };
+    
+    public static void main(String args[]) {
+	ServerSocket serverSocket = null;
+	Socket socket = null;
+	try {
+	    serverSocket = new ServerSocket(SERVER_PORT);
+	    while (true) {
+		System.out.println("-- simple SampleServer start");
+		socket = serverSocket.accept();
+                BufferedWriter bufwriter
+		    = new BufferedWriter(
+					 new OutputStreamWriter(socket.getOutputStream()));
+		for(int i=0; i<messages.length; i++ ){
+		    bufwriter.write(messages[i], 0, messages[i].length());
+		    bufwriter.newLine();
 		}
+		bufwriter.close();
+		socket.close();
+		System.out.println("-- simple SampleServer end");
+	    }
+	}catch (IOException e) {
+	    e.printStackTrace();
 	}
+    }
 }
